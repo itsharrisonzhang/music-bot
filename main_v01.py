09b81b4a5a235b6c1fdd508d60770aeb0667bf10
Harrison Zhang <hzhang3839@gmail.com>
2022-08-14 18:46:41 -0700

    added git path to discord.py

--- main_v01.py
+++ main_v01.py
@@ -6,6 +6,7 @@ import threading
 import urllib.request, re
 
 client = commands.Bot(command_prefix = "/")
+
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 YDL_OPTIONS = {'format' : "bestaudio"}
 
